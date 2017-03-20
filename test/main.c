99a4400828d759df5fcc7da79f6a92258f8a16d6
Lisa <110@holbetonschool.com>
2017-03-20 22:58:39 +0000

    main.c changes

--- test/main.c
+++ test/main.c
@@ -40,5 +40,7 @@ int main(void)
 	printf("Len:[%d]\n", len2);
 	//_printf("Unknown:[%r]\n");
 	//printf("Unknown:[%r]\n");
+	_printf("Unsigned:[%u]\n", ui);
+        printf("Unsigned:[%u]\n", ui);
 	return (0);
 }
