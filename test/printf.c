956d791e59b0ffd3aaf75ed3f6e5d3ca34ca858f
Kimberly Wong <kjowong@gmail.com>
2017-03-21 05:30:56 +0000

    Condensed code for style fix

--- test/printf.c
+++ test/printf.c
@@ -1,36 +1,27 @@
 #include <stdarg.h>
 #include "holberton.h"
 #include <stdio.h>
-
+/**
+  * _printf - function that prints based on format specifier
+  * @format: takes in format specifier
+  * Return: return pointer to index
+  */
 int _printf(const char *format, ...)
 {
 	char buffer[1024];
-	int i, j, a;
+	int i = 0, j = 0, a = 0;
 	va_list valist;
-	int *index;
+	int *index = &a;
 	vtype_t spec[] = {
-		{'c', format_c},
-		{'d', format_d},
-		{'s', format_s},
-		{'i', format_i},
-		{'u', format_u},
-		{'%', format_perc},
-		{'x', format_h},
-		{'X', format_ch},
-		{'o', format_o},
-		{'b', format_b},
-		{'\0', NULL}
+		{'c', format_c}, {'d', format_d}, {'s', format_s}, {'i', format_i},
+		{'u', format_u}, {'%', format_perc}, {'x', format_h}, {'X', format_ch},
+		{'o', format_o}, {'b', format_b}, {'\0', NULL}
 	};
-	i = 0;
-	j = 0;
-	a = 0;
 
-	index = &a;
-	//printf("INDEX %d", *index);
 	va_start(valist, format);
 	while (format[i] != '\0')
 	{
-		while (format[i] != '%' &&  format[i] != '\0')
+		for (; format[i] != '%' && format[i] != '\0'; *index += 1, i++)
 		{
 			if (*index == 1024)
 			{
@@ -39,31 +30,25 @@ int _printf(const char *format, ...)
 				*index = 0;
 			}
 			buffer[*index] = format[i];
-			i++;
-			*index += 1;
 		}
 		if (format[i] == '\0')
-		{
 			break;
-		}
-
 		if (format[i] == '%')
 		{
 			i++;
-			j = 0;
-			while (spec[j].tp != '\0')
+			for (j = 0; spec[j].tp != '\0'; j++)
 			{
 				if (format[i] == spec[j].tp)
 				{
 					spec[j].f(valist, buffer, index);
 					break;
 				}
-				j++;
 			}
 		}
 		i++;
 	}
 	/*null terminator to buffer for testing purposes*/
+	va_end(valist);
 	buffer[*index] = '\0';
 	_write_buffer(buffer, index);
 	return (*index);
