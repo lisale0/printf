40ad025ff621053eb2d93588e475fe409176ec3c
Lisa <110@holbetonschool.com>
2017-03-21 17:11:56 +0000

    style fix for copt_function

--- test/copy_functions.c
+++ test/copy_functions.c
@@ -3,23 +3,30 @@
 #include <stdlib.h>
 
 
-
+/**
+ * format_i - specificer i
+ * @valist: valist
+ * @buffer: buffer
+ * @index: index
+ *
+ * Return: void
+ */
 void format_i(va_list valist, char *buffer, int *index)
 {
 	int i, j, numlen;
-        i = va_arg(valist, int);
 
+	i = va_arg(valist, int);
 	numlen = num_len(i);
-
 	if (i < 0)
 	{
 		*index += 1;
 		numlen += 1;
 	}
 	char num_str[numlen];
+
 	itos(num_str, i);
 	for (i = *index, j = 0; j < numlen; *index += 1, i++, j++)
-        {
+	{
 		if (*index == 1024)
 		{
 			_write_buffer(buffer, index);
@@ -27,30 +34,32 @@ void format_i(va_list valist, char *buffer, int *index)
 			*index = 0;
 		}
 		buffer[*index] = num_str[j];
-        }
+	}
 }
 /**
+ * format_d - specificer d
+ * @valist: valist
+ * @buffer: buffer
+ * @index: index
  *
- *
- *
- *
+ * Return: void
  */
 void format_d(va_list valist, char *buffer, int *index)
 {
 	int i, j, numlen;
-        i = va_arg(valist, int);
+
+	i = va_arg(valist, int);
 
 	numlen = num_len(i);
 	if (i < 0)
-        {
-                //*index += 1;
-                numlen += 1;
-        }
-        char num_str[numlen];
+	{
+		numlen += 1;
+	}
+	char num_str[numlen];
 
 	itos(num_str, i);
 	for (i = *index, j = 0; j < numlen; *index += 1, i++, j++)
-        {
+	{
 		if (*index == 1024)
 		{
 			_write_buffer(buffer, index);
@@ -58,16 +67,16 @@ void format_d(va_list valist, char *buffer, int *index)
 			*index = 0;
 		}
 		buffer[*index] = num_str[j];
-        }
+	}
 }
 /**
-  * copy_string - function that copies string to buffer
-  * @valist: takes in a list
-  * @buffer: takes in a buffer
-  * @index: takes in the index
-  *
-  * Return: void
-  */
+ * format_s - specificer s
+ * @valist: valist
+ * @buffer: buffer
+ * @index: index
+ *
+ * Return: void
+ */
 void format_s(va_list valist, char *buffer, int *index)
 {
 	int i, j, strlen;
@@ -79,27 +88,27 @@ void format_s(va_list valist, char *buffer, int *index)
 	for (i = *index, j = 0; s[j] != '\0';  *index += 1, i++, j++)
 	{
 		if (*index == 1024)
-                {
-                        _write_buffer(buffer, index);
-                        reset_buffer(buffer);
+		{
+			_write_buffer(buffer, index);
+			reset_buffer(buffer);
 			*index = 0;
-                }
+		}
 		buffer[*index] = s[j];
 	}
 }
 /**
- * copy_char - function that copies a char to buffer
- * @valist: takes in list
- * @buffer: takes in a buffer
- * @index: takes in index
+ * format_c - specificer c
+ * @valist: valist
+ * @buffer: buffer
+ * @index: index
  *
  * Return: void
  */
 void format_c(va_list valist, char *buffer, int *index)
 {
-        char s;
+	char s;
 
-        s = va_arg(valist, int);
+	s = va_arg(valist, int);
 	if (*index == 1024)
 	{
 		_write_buffer(buffer, index);
