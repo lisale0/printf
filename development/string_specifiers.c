c44043fe3383dc4defaea6beaef254d312b943d4
Lisa <110@holbetonschool.com>
2017-03-22 02:02:18 +0000

    removed duplicate functions

--- development/string_specifiers.c
+++ development/string_specifiers.c
@@ -1,143 +1,51 @@
+#include "holberton.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * itos - integer to string
- * @str: char array
- * @n: number
+ * format_s - specificer s
+ * @valist: valist
+ * @buffer: buffer
+ * @index: index
  *
- * Return: char array
+ * Return: void
  */
-char *itos(char str[], long int n)
+void format_s(va_list valist, char *buffer, int *index)
 {
-	int i, temp, div, length, last;
+	int i, j;
+	char *s;
 
-	div = 10;
-	last = n % 10;
-	i = 0;
-	if (last < 0)
+	s = va_arg(valist, char*);
+
+	for (i = *index, j = 0; s[j] != '\0';  *index += 1, i++, j++)
 	{
-		last *= -1;
-		str[i] = '-';
-		i++;
-	}
-	n = (n / 10);
-	if (n < 0)
-	{
-		n *= -1;
-	}
-
-	length = 0;
-	div = 1;
-	temp = n;
-
-	while (temp > 0)
-	{
-		length += 1;
-		div *= 10;
-		temp = temp / 10;
-	}
-
-	div = div / 10;
-	while (div >= 1)
-	{
-		str[i] = ((n / div) + '0');
-		n = n % div;
-		div /= 10;
-		i++;
-	}
-	str[i] = last + '0';
-	return (str);
-}
-
-/**
- * utos - uint to string
- * @str: string
- * @num: num
- *
- * Return: char array
- */
-char *utos(char str[], unsigned int num)
-{
-	int i, rem, len = 0, n;
-
-	n = num;
-	while (n != 0)
-	{
-		len++;
-		n /= 10;
-	}
-	i = 0;
-
-	while (i < len)
-	{
-		rem = num % 10;
-		num = num / 10;
-
-		str[len - (i + 1)] = rem + '0';
-		i++;
-	}
-
-	str[len] = '\0';
-	return (str);
-}
-/**
- * num_len - getting the length of a number
- * @num: the number
- *
- * Return: int
- */
-int num_len(int num)
-{
-	int n, len;
-
-	n = num;
-	len = 0;
-	while (n != 0)
-	{
-		len++;
-		n /= 10;
-	}
-	return (len);
-}
-
-/**
-* rev_string = reverses a string
-* @takes in a string
-*/
-
-void rev_string(char *s)
-{
-	int i, c;
-	char temp;
-
-	c = 0;
-	while (s[c] != '\0')
-	{
-		c++;
-	}
-	c--;
-
-	for (i = 0; i < c; i++)
-	{
-		temp = s[i];
-		s[i] = s[c];
-		s[c] = temp;
+		if (*index == 1024)
+		{
+			_write_buffer(buffer, index);
+			reset_buffer(buffer);
+			*index = 0;
+		}
+		buffer[*index] = s[j];
 	}
 }
 /**
- * _strlen - count the length of a string
- *  @s: char pointer
+ * format_c - specificer c
+ * @valist: valist
+ * @buffer: buffer
+ * @index: index
  *
- * Return: integer
+ * Return: void
  */
-int _strlen(char *s)
+void format_c(va_list valist, char *buffer, int *index)
 {
-	int i, count;
+	char s;
 
-	count = 0;
-	for (i = 0; s[i] != '\0'; i++)
+	s = va_arg(valist, int);
+	if (*index == 1024)
 	{
-		count++;
+		_write_buffer(buffer, index);
+		reset_buffer(buffer);
+		*index = 0;
 	}
-	return (i);
+	buffer[*index] = s;
+	*index += 1;
 }
