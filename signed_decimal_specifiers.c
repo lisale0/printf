af9d3f0560d95b39bed46ca5e24e1b6208652006
Lisa <110@holbetonschool.com>
2017-03-22 04:50:50 +0000

    removed format_i

--- signed_decimal_specifiers.c
+++ signed_decimal_specifiers.c
@@ -2,41 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * format_i - specificer i
- * @valist: valist
- * @buffer: buffer
- * @index: index
- *
- * Return: void
- */
-void format_i(va_list valist, char *buffer, int *index)
-{
-	int i, j, numlen;
-	char *num_str;
-
-	i = va_arg(valist, int);
-	numlen = num_len(i);
-	if (i < 0)
-	{
-		*index += 1;
-		numlen += 1;
-	}
-	num_str = malloc(numlen * sizeof(char));
-	if (num_str == NULL)
-		return;
-	itos(num_str, i);
-	for (i = *index, j = 0; j < numlen; *index += 1, i++, j++)
-	{
-		if (*index == 1024)
-		{
-			_write_buffer(buffer, index);
-			reset_buffer(buffer);
-			*index = 0;
-		}
-		buffer[*index] = num_str[j];
-	}
-}
-/**
  * format_d - specificer d
  * @valist: valist
  * @buffer: buffer
