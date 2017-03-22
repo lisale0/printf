545363938fd0304b590179acda4422d1fcc7ff41
Lisa <110@holbetonschool.com>
2017-03-22 04:53:59 +0000

    changed format_i to format_d

--- printf.c
+++ printf.c
@@ -10,7 +10,7 @@ int _printf(const char *format, ...)
 	int i, j = 0, a = 0, *index = &a;
 	va_list valist;
 	vtype_t spec[] = {
-		{'c', format_c}, {'d', format_d}, {'s', format_s}, {'i', format_i},
+		{'c', format_c}, {'d', format_d}, {'s', format_s}, {'i', format_d},
 		{'u', format_u}, {'%', format_perc}, {'x', format_h}, {'X', format_ch},
 		{'o', format_o}, {'b', format_b}, {'p', format_p}, {'r', format_r},
 		{'R', format_R}, {'\0', NULL}
